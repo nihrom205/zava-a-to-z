c01220f46a5fc3a59147370283e0dfe01dd0ec9e
Alexey Rastorguev <rastorguev00@gmail.com>
2017-09-15 17:39:58 +0500

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+* Class Calculate выводит строку "Hello World"
+* 
+* @author Alexey Rastorguev (mailto:rastorguev00@gmail.com)
+* @version $ld$
+* @since 15.09.2017
+*/
 public class Calculate {
+	/**
+	* Вывод строки в консоль
+	* @param args - args.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Alexey Rastorguev (mailto:rastorguev00@gmail.com)
+* @version $ld$
+* @since 15.09.2017
+*/
+
+package ru.java-a-to-z;
