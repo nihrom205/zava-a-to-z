348e89cc07fe84abee30f7651e8baca5e4dafcd0
Alexey Rastorguev <rastorguev00@gmail.com>
2017-11-17 17:33:14 +0500

    modify chapter_002, add chapter_003

--- chapter_002/src/main/java/ru/job4j/collectionssort/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Package collectionssort.
- *
- * @author Alexey Rastorguev (rastorguev00@gmail.com)
- * @version 0.1
- * @since 12.11.2017
- */
-package ru.job4j.collectionssort;
--- chapter_002/src/test/java/ru/job4j/collections/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Testing package collections.
- *
- * @author Alexey Rastorguev (rastorguev00@gmail.com)
- * @version 0.1
- * @since 11.11.2017
- */
-package ru.job4j.collections;
