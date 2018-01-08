f16acafa213a2847c6ff65ea998c97a7008f996a
Alexey Rastorguev <rastorguev00@gmail.com>
2018-01-08 21:54:19 +0500

    modify Node, package tree.

--- chapter_005/src/main/java/ru/job4j/tree/Node.java
+++ chapter_005/src/main/java/ru/job4j/tree/Node.java
@@ -32,8 +32,12 @@ public class Node<E> {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Node<?> node = (Node<?>) o;
 
