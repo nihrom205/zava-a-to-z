1ec065bf65cf1ee1bf85ffb4d87bdd2d1938bd20
Alexey Rastorguev <rastorguev00@gmail.com>
2018-01-25 15:32:42 +0500

    add Find space and word in text [task #1016]

--- chapter_006/src/main/java/ru/job4j/threads/FoundWordSpace.java
+++ chapter_006/src/main/java/ru/job4j/threads/FoundWordSpace.java
@@ -3,42 +3,65 @@ package ru.job4j.threads;
 /**
  * Class FoundWordSpace.
  *
+ * Класс подсчитывает кол-во слова в тексте и кол-во пробелов.
+ *
  * @author Alexey Rastorguev (rastorguev00@gmail.com)
  * @version 0.1
  * @since 22.01.2018
  */
 public class FoundWordSpace {
     private String text;
+    private int countWord = 0;
+    private int countSpace = 0;
 
     public FoundWordSpace(String text) {
         this.text = text;
     }
 
+    // Геттеры ======================
+    public int getCountWord() {
+        return countWord;
+    }
+
+    public int getCountSpace() {
+        return countSpace;
+    }
+    //=============================
+
+    /**
+     * метод подчета слов и пробелов
+     */
     public void runFind() {
-        new Thread() {
+        new Thread("thredWord") {
             @Override
             public void run() {
-                int countWord = 0;
+                boolean isWord = false;
                 for (int i = 0; i < text.length(); i++) {
                     if (text.charAt(i) >= 65 && text.charAt(i) <= 122) {
+                        isWord = true;
+                    } else if (text.charAt(i) == 32 && isWord) {
                         countWord++;
+                        isWord = false;
                     }
                 }
-                System.out.println("Кол-во букв = " + countWord);
+                if (isWord) {
+                    countWord++;
+                }
+//                System.out.println("Кол-во букв = " + countWord);
             }
         }.start();
 
-        new Thread() {
+        new Thread("thredSpace") {
             @Override
             public void run() {
-                int countSpace = 0;
                 for (int i = 0; i < text.length(); i++) {
                     if (text.charAt(i) == 32) {
                         countSpace++;
                     }
                 }
-                System.out.println("Кол-во пробелов = " + countSpace);
             }
         }.start();
     }
+
+
 }
--- chapter_006/src/test/java/ru/job4j/threads/FoundWordSpaceTest.java
+++ chapter_006/src/test/java/ru/job4j/threads/FoundWordSpaceTest.java
@@ -2,10 +2,12 @@ package ru.job4j.threads;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 /**
- * Class <Name class>.
+ * Class FoundWordSpaceTest.
+ * Тестирование класса FoundWordSpace.
  *
  * @author Alexey Rastorguev (rastorguev00@gmail.com)
  * @version 0.1
@@ -14,17 +16,14 @@ import static org.junit.Assert.*;
 public class FoundWordSpaceTest {
     @Test
     public void whenFindWordAndSpaceThenCountWordAndSpace() {
-//        FoundWordSpace fount = new FoundWordSpace(" AZaza vv dd");
-//        fount.runFind();
-
-
-        String text = " AZaza vv dd";
-        int countSpace = 0;
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == 32) {
-                countSpace++;
-            }
+        FoundWordSpace found = new FoundWordSpace("AZaza vv dd");
+        found.runFind();
+        try {
+            Thread.sleep(1000);
+            assertThat(found.getCountWord(), is(3));
+            assertThat(found.getCountSpace(), is(2));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.println("Кол-во пробелов = " + countSpace);
     }
 }
