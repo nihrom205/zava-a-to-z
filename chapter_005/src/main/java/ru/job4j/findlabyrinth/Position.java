7a72cb9427b1756d9d7315654e2fe0de3677049d
Alexey Rastorguev <rastorguev00@gmail.com>
2018-03-04 15:31:59 +0500

    modify find from start to finish (labyrinth)

--- chapter_005/src/main/java/ru/job4j/findlabyrinth/Position.java
+++ chapter_005/src/main/java/ru/job4j/findlabyrinth/Position.java
@@ -39,12 +39,18 @@ public class Position {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Position position = (Position) o;
 
-        if (x != position.x) return false;
+        if (x != position.x) {
+            return false;
+        }
         return y == position.y;
     }
 
